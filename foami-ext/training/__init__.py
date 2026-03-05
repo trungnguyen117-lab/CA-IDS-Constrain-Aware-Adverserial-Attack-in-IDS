d8f067fc8d650377ef46680ad0b84bb7b02c1b2e
Nguyen Trung Nguyen 22024553 <146165972+trungnguyen117-lab@users.noreply.github.com>
2026-03-05 15:13:25 +0700

    Delete foami-ext/training directory

--- foami-ext/training/__init__.py
+++
@@ -1,7 +0,0 @@
-from .xgb   import XGBModel
-from .catb  import CatBoostModel
-from .rf    import RFModel
-from .lstm  import LSTMModel
-from .resdnn import ResDNNModel
-
-__all__ = ['XGBModel', 'CatBoostModel', 'RFModel', 'LSTMModel', 'ResDNNModel']
